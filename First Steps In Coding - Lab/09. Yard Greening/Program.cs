10beb3ebf454c64cfd83cfe2274e79e9b4e28750
GeorgePachedjiev <82244895+GeorgePachedjiev@users.noreply.github.com>
2022-06-23 10:26:05 -0900

    Yard Greening(exercise)

---
+++ First Steps In Coding - Lab/09. Yard Greening/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _09._Yard_Greening
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double kvmetar = 7.61;
+            double metri = double.Parse(Console.ReadLine());
+            double cqlacena = kvmetar * metri;
+            double otstapka = (metri * kvmetar) * 0.18;
+            Console.WriteLine("The final price is: " + (cqlacena - otstapka) + " lv.");
+            Console.WriteLine("The discound is: " + otstapka + " lv.");
+        }
+    }
+}
