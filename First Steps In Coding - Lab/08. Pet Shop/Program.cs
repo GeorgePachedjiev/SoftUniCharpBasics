4bda347e9e5c022d6a2366c2245b5f515a89a566
GeorgePachedjiev <82244895+GeorgePachedjiev@users.noreply.github.com>
2022-06-23 10:25:03 -0900

    Pet Shop(exercise)

---
+++ First Steps In Coding - Lab/08. Pet Shop/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _08._Pet_Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double kuce = 2.5;
+            double kote = 4;
+            int broikuce = int.Parse(Console.ReadLine());
+            int broikote = int.Parse(Console.ReadLine());
+            Console.WriteLine((broikuce * kuce) + (broikote * kote) + " lv.");
+        }
+    }
+}
