dd99a82737513264a941e18cfe6d30d496cbde3b
GeorgePachedjiev <82244895+GeorgePachedjiev@users.noreply.github.com>
2022-06-24 10:32:10 -0900

    Fish Tank

---
+++ First Steps In Coding - Exercise/09.Fish Tank/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Fish_Tank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dalshina = double.Parse(Console.ReadLine());
+            double shirina = double.Parse(Console.ReadLine());
+            double visochina = double.Parse(Console.ReadLine());
+            double procent = double.Parse(Console.ReadLine());
+            double obem = dalshina * shirina * visochina;
+            double obemltr = obem / 1000;
+            double zaeto = procent / 100;
+            double sum = obemltr * (1 - zaeto);
+                Console.WriteLine(sum);
+        }
+    }
+}
