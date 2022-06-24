c1fe7cc08669e0644e92e3a7f09886ef45c76086
GeorgePachedjiev <82244895+GeorgePachedjiev@users.noreply.github.com>
2022-06-24 10:29:43 -0900

    Supplies for School

---
+++ First Steps In Coding - Exercise/05.Supplies for School/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Supplies_for_School
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pakethimikali = 5.80;
+            double paketmarkeri = 7.20;
+            double preparatlit = 1.20;
+            double broihim = int.Parse(Console.ReadLine());
+            double broimar = int.Parse(Console.ReadLine());
+            double prep = int.Parse(Console.ReadLine());
+            double nam = int.Parse(Console.ReadLine());
+            double cenpakhim = broihim * pakethimikali;
+            double cenpakmar = broimar* paketmarkeri;
+            double cenpakprep = prep * preparatlit;
+            double sum = cenpakhim + cenpakmar + cenpakprep;
+            //double nam1 = nam % 100;
+
+                Console.WriteLine(sum - ((sum * nam)/100));
+        }
+    }
+}
