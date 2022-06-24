42bb9abbb86e333c643904d2def5422f68f337fb
GeorgePachedjiev <82244895+GeorgePachedjiev@users.noreply.github.com>
2022-06-24 10:28:38 -0900

    Deposit Calculator

---
+++ First Steps In Coding - Exercise/03.Deposit Calculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Deposit_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double depsum = double.Parse(Console.ReadLine());
+            double srokdep = double.Parse(Console.ReadLine());
+            double lihva = double.Parse(Console.ReadLine());
+
+            double natlih = depsum * lihva;
+            double lihedinmsc = natlih / 12;
+            double obsto = depsum + srokdep * lihedinmsc;
+            
+            Console.WriteLine(obsto);
+
+
+        }
+    }
+}
