f739d0a4fad7f31a32afda23c57fa880d68bedd9
GeorgePachedjiev <82244895+GeorgePachedjiev@users.noreply.github.com>
2022-06-24 10:28:57 -0900

    Vacancion Books

---
+++ First Steps In Coding - Exercise/04.Vacation Books List/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Vacation_Books_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int broistr = int.Parse(Console.ReadLine());
+            int str = int.Parse(Console.ReadLine());
+            int broidni = int.Parse(Console.ReadLine());
+            int obstovreme = broistr / str;
+            int sum = obstovreme / broidni;
+            Console.WriteLine(sum);
+        }
+    }
+}
