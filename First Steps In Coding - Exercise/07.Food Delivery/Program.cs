d54c9b5db536879d4516ebc21ab350d599f4f501
GeorgePachedjiev <82244895+GeorgePachedjiev@users.noreply.github.com>
2022-06-24 10:30:22 -0900

    Food Delivery

---
+++ First Steps In Coding - Exercise/07.Food Delivery/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Food_Delivery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cmenu = int.Parse(Console.ReadLine());
+            int fmenu = int.Parse(Console.ReadLine());
+            int vmenu = int.Parse(Console.ReadLine());
+            double amount = cmenu * 10.35 + fmenu * 12.40 + vmenu * 8.15;
+            double dessert = amount * 0.20;
+            Console.WriteLine(amount + dessert + 2.50);
+        }
+    }
+}
