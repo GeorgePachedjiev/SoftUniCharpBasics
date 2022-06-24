4f5694f00fd8c47bd2ace31226284403145613ba
GeorgePachedjiev <82244895+GeorgePachedjiev@users.noreply.github.com>
2022-06-24 10:30:07 -0900

    Repainting

---
+++ First Steps In Coding - Exercise/06.Repainting/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Repainting1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nailon = int.Parse(Console.ReadLine());
+            int paint = int.Parse(Console.ReadLine());
+            int razr = int.Parse(Console.ReadLine());
+            int hours = int.Parse(Console.ReadLine());
+            double price = nailon * 1.50 + paint * 14.50 + razr * 5.00 + 0.10 * paint * 14.50 + 2 * 1.50 + 0.40;
+            double amount = price * 0.30 * hours;
+            double alltogether = price + amount;
+            Console.WriteLine(alltogether);
+        }
+    }
+}
