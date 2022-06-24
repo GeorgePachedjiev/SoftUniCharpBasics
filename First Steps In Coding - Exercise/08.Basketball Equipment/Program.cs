23ee4220b8bdd120af0049627b1aaeedd558b7e3
GeorgePachedjiev <82244895+GeorgePachedjiev@users.noreply.github.com>
2022-06-24 10:31:14 -0900

    Basketball Equipment

---
+++ First Steps In Coding - Exercise/08.Basketball Equipment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Basketball_Equipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cena = int.Parse(Console.ReadLine());
+            double kecove = cena - 0.4 * cena;
+            double ekip = kecove - 0.2 * kecove;
+            double topki = ekip / 4;
+            double accesoari = topki / 5;
+            double obshto = cena + kecove + ekip + topki + accesoari;
+            Console.WriteLine(obshto);
+
+        }
+    }
+}
