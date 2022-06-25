b00c83cef8a493fbc03607ab41d7a9284b24eb4f
GeorgePachedjiev <82244895+GeorgePachedjiev@users.noreply.github.com>
2022-06-25 02:05:51 -0900

    Area of Figures

---
+++ Conditional Statements/07.Area of Figures/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Area_of_Figures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string izbor = Console.ReadLine();
+           
+            if(izbor == "square")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double area = a * a;
+                Console.WriteLine(area);
+            }
+            if (izbor == "rectangle")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double b = double.Parse(Console.ReadLine());
+                double area = (a * b);
+                Console.WriteLine(area); 
+            }
+            if (izbor == "circle")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double area = (a * a * Math.PI);
+                Console.WriteLine(area);
+            }
+            if (izbor == "triangle")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double b = double.Parse(Console.ReadLine());
+                double area = (a * b / 2);
+                Console.WriteLine(area);
+            }
+        }
+    }
+}
