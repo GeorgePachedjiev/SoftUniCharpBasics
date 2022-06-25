1d4de4be5932269acb9c013c728a70504dab228b
GeorgePachedjiev <82244895+GeorgePachedjiev@users.noreply.github.com>
2022-06-25 02:04:04 -0900

    Even or Odd

---
+++ Conditional Statements/03.Even or Odd/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Even_or_Odd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int chislo = int.Parse(Console.ReadLine());
+            if (chislo % 2 == 0)
+            {
+                Console.WriteLine("even");
+            }
+            else
+            {
+                Console.WriteLine("odd");
+            }
+        }
+    }
+}
