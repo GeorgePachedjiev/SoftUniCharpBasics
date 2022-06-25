62ed47548a7d25191824d356d27059905f77219c
GeorgePachedjiev <82244895+GeorgePachedjiev@users.noreply.github.com>
2022-06-25 02:05:22 -0900

    Speed Info

---
+++ Conditional Statements/06.Speed Info/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Speed_Info
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double km = double.Parse(Console.ReadLine());
+            if (km <= 10)
+            {
+                Console.WriteLine("slow");
+            }
+            if (km > 10 && km <=50)
+            {
+                Console.WriteLine("average");
+            }
+            if (km > 50 && km <= 150)
+            {
+                Console.WriteLine("fast");
+            }
+            if (km > 150 && km <= 1000)
+            {
+                Console.WriteLine("ultra fast");
+            }
+            if (km > 1000)
+            {
+                Console.WriteLine("extremely fast");
+            }
+        }
+    }
+}
