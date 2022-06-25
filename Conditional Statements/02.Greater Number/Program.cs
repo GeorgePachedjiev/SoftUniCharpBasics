4810da5ec2f08df5c6d5a7c06618178e205241fc
GeorgePachedjiev <82244895+GeorgePachedjiev@users.noreply.github.com>
2022-06-25 02:03:32 -0900

    Greater Number

---
+++ Conditional Statements/02.Greater Number/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Greater_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int parvochislo = int.Parse(Console.ReadLine());
+            int vtorochislo = int.Parse(Console.ReadLine());
+            if (parvochislo > vtorochislo)
+            {
+                Console.WriteLine(parvochislo);
+            }
+            else
+            {
+                Console.WriteLine(vtorochislo);
+            }
+        }
+    }
+}
