599ccdfb5ab7657fa3de27f361e9029c1c0f97f4
GeorgePachedjiev <82244895+GeorgePachedjiev@users.noreply.github.com>
2022-06-25 02:04:56 -0900

    Number 100...200

---
+++ Conditional Statements/05.Number 100__200/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Number_100__200
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int chislo = int.Parse(Console.ReadLine());
+            if(chislo < 100)
+            {
+                Console.WriteLine("Less than 100");
+            }
+            if ( chislo >= 100 && chislo <= 200)
+            {
+                Console.WriteLine("Between 100 and 200");
+            }
+            if (chislo > 200)
+            {
+                Console.WriteLine("Greater than 200");
+            }
+        }
+    }
+}
