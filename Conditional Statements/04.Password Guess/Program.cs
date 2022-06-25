5de88a78f5f4785ee256f98b67d43161d525a18e
GeorgePachedjiev <82244895+GeorgePachedjiev@users.noreply.github.com>
2022-06-25 02:04:35 -0900

    Password Guess

---
+++ Conditional Statements/04.Password Guess/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Password_Guess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pass = Console.ReadLine();
+            string password = "s3cr3t!P@ssw0rd)";
+            if(pass == password)
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
