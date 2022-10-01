3a87ca2cd4bac3cb4a9a6fbfee42837a6605072e
GeorgePachedjiev <82244895+GeorgePachedjiev@users.noreply.github.com>
2022-10-01 05:07:45 -0900

    Godzilla vs Kong

---
+++ Conditional Statements - Exercise/01. Sum Seconds/05. Godzilla vs. Kong/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _05._Godzilla_vs._Kong
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budge = double.Parse(Console.ReadLine());
+            int broistatisti = int.Parse(Console.ReadLine());
+            double cenanaobleklo = double.Parse(Console.ReadLine());
+            double cenadecor = 0.10 * budge;
+            if (broistatisti > 150)
+            {
+                cenanaobleklo *= 0.90;
+            }
+            double raznoski = cenanaobleklo * broistatisti + cenadecor;
+            if (budge < raznoski)
+            {
+                double need = raznoski - budge;
+                Console.WriteLine($"Not enough money!");
+                Console.WriteLine($"Wingard needs {need:f2} leva more.");
+            }
+            else
+            {
+                double need1 = budge - raznoski;
+                Console.WriteLine($"Action!");
+                Console.WriteLine($"Wingard starts filming with {need1:f2} leva left.");
+            }
+        }
+    }
+}
