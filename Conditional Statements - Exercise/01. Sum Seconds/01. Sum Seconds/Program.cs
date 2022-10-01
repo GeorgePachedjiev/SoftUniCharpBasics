94958b6314575e9ebf155fb8d0048a07fc6eeb98
GeorgePachedjiev <82244895+GeorgePachedjiev@users.noreply.github.com>
2022-10-01 04:59:08 -0900

    01. Sum Seconds

---
+++ Conditional Statements - Exercise/01. Sum Seconds/01. Sum Seconds/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _01._Sum_Seconds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int parvi = int.Parse(Console.ReadLine());
+            int vtori = int.Parse(Console.ReadLine());
+            int treti = int.Parse(Console.ReadLine());
+            int obsto = parvi + vtori + treti;
+            int minuti = obsto / 60;
+            int sekundi = obsto % 60;
+            if (sekundi < 10)
+            {
+                Console.WriteLine($"{minuti}:0{sekundi}");
+            }
+            else
+            {
+                Console.WriteLine($"{minuti}:{sekundi}");
+            }
+        }
+    }
+}
