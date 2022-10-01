529da05f5335ff6a2021fc777c599a04eb301330
GeorgePachedjiev <82244895+GeorgePachedjiev@users.noreply.github.com>
2022-10-01 05:10:37 -0900

    LunchBreakExercise

---
+++ Conditional Statements - Exercise/01. Sum Seconds/08. Lunch Break/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _08._Lunch_Break
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string serial = Console.ReadLine();
+            double vremeNaepizoda = double.Parse(Console.ReadLine());
+            double vremepochivka = double.Parse(Console.ReadLine());
+
+            double vremeobqd = 0.125 * vremepochivka;
+            double vremeodih = 0.25 * vremepochivka;
+
+            double ostanaloVreme = vremepochivka - vremeobqd - vremeodih;
+            double ostanaloVreme1 = ostanaloVreme - vremeNaepizoda;
+            double trqbvaNivreme = vremeNaepizoda - ostanaloVreme;
+            if (ostanaloVreme >= vremeNaepizoda)
+            {
+                Console.WriteLine($"You have enough time to watch {serial} and left with {Math.Ceiling(ostanaloVreme1)} minutes free time.");
+            }
+            else
+            {
+                Console.WriteLine($"You don't have enough time to watch {serial}, you need {Math.Ceiling(trqbvaNivreme)} more minutes.");
+            }
+        }
+    }
+}
