3c439b980f4f3b5b69e636d52061d15e8b2680db
GeorgePachedjiev <82244895+GeorgePachedjiev@users.noreply.github.com>
2022-10-01 05:03:16 -0900

    SumSecondsExercise

---
+++ Conditional Statements - Exercise/01. Sum Seconds/02. Bonus Score/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _02._Bonus_Score
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int chislo = int.Parse(Console.ReadLine());
+            double bonus = 0;
+            if (chislo <= 100)
+            {
+                bonus = 5;
+            }
+            else if (chislo > 1000)
+            {
+                bonus = chislo * 0.10;
+            }
+
+            else
+            {
+                bonus = chislo * 0.20;
+            }
+
+
+            if (chislo % 2 == 0)
+            {
+                bonus += 1;
+            }
+            else if (chislo % 10 == 5)
+            {
+                bonus += 2;
+            }
+            Console.WriteLine(bonus);
+            Console.WriteLine(chislo + bonus);
+        }
+    }
+}
