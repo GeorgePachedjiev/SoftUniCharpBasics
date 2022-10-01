db556220ddaf6dc4e588b3bcdf9e62d21251d88c
GeorgePachedjiev <82244895+GeorgePachedjiev@users.noreply.github.com>
2022-10-01 05:08:50 -0900

    Swimming World Record

---
+++ Conditional Statements - Exercise/01. Sum Seconds/06. World Swimming Record/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _06._World_Swimming_Record
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //recorda v sekundi
+            double record = double.Parse(Console.ReadLine());
+            //raztoqnieto v metri
+            double distance = double.Parse(Console.ReadLine());
+            //vremeto v sekundi za koeto pluva 1 metar
+            double metarVSekunda = double.Parse(Console.ReadLine());
+
+            double vreme = distance * metarVSekunda;
+            double naPetNaiSeMetra = Math.Floor(distance / 15) * 12.5;
+            double obstoVreme = vreme + naPetNaiSeMetra;
+
+            double vremeKoetoNeEDostignalo = obstoVreme - record;
+            double podoBrenoVreme = record - obstoVreme;
+            if (obstoVreme >= record)
+            {
+                Console.WriteLine($"No, he failed! He was {vremeKoetoNeEDostignalo:f2} seconds slower.");
+            }
+            else
+            {
+                Console.WriteLine($"Yes, he succeeded! The new world record is {obstoVreme:f2} seconds.");
+            }
+        }
+    }
+}
