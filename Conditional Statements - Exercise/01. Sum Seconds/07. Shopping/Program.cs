2ab8a8f0b116fa97abbed09130af3f055c963fc3
GeorgePachedjiev <82244895+GeorgePachedjiev@users.noreply.github.com>
2022-10-01 05:09:44 -0900

    ShoppingExercise

---
+++ Conditional Statements - Exercise/01. Sum Seconds/07. Shopping/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _07._Shopping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budge = double.Parse(Console.ReadLine());
+            double broivideokarti = double.Parse(Console.ReadLine());
+            double broiprocesori = double.Parse(Console.ReadLine());
+            double broiram = double.Parse(Console.ReadLine());
+
+            const double videocartacena = 250;
+            double videoCenaobsto = videocartacena * broivideokarti;
+            double procesor = 0.35 * videoCenaobsto;
+            double ramobsto = 0.10 * videoCenaobsto;
+
+            double obstaSmetka = broivideokarti * videocartacena + broiprocesori * procesor + broiram * ramobsto;
+
+            if (broivideokarti > broiprocesori)
+            {
+                obstaSmetka *= 0.85;
+            }
+            double ostatak = budge - obstaSmetka;
+            double nushnasuma = obstaSmetka - budge;
+            if (budge >= obstaSmetka)
+            {
+                Console.WriteLine($"You have {ostatak:f2} leva left!");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! You need {nushnasuma:f2} leva more!");
+            }
+        }
+    }
+}
