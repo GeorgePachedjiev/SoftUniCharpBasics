2e0dc25bd36328d2ebc278033bdb88eb7de37f73
GeorgePachedjiev <82244895+GeorgePachedjiev@users.noreply.github.com>
2022-10-02 02:54:38 -0900

    Personal Titles

--- Conditional Statements Advanced - Lab/01. Day of Week/03. Animal Type/Program.cs
+++ Conditional Statements Advanced - Lab/01. Day of Week/03. Animal Type/Program.cs
@@ -6,7 +6,21 @@ namespace _03._Animal_Type
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string animalType = Console.ReadLine();
+            switch (animalType)
+            {
+                case "dog":
+                    Console.WriteLine("mammal");
+                    break;
+                case "crocodile":
+                case "tortoise":
+                case "snake":
+                    Console.WriteLine("reptile");
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
         }
     }
 }
