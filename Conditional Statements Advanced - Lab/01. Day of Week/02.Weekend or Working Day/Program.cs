67415f9ac227e951cd51c7dfba7f84718cbd29cd
GeorgePachedjiev <82244895+GeorgePachedjiev@users.noreply.github.com>
2022-10-02 02:53:30 -0900

    AnimalType

--- Conditional Statements Advanced - Lab/01. Day of Week/02.Weekend or Working Day/Program.cs
+++ Conditional Statements Advanced - Lab/01. Day of Week/02.Weekend or Working Day/Program.cs
@@ -6,7 +6,27 @@ namespace _02.Weekend_or_Working_Day
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string day = Console.ReadLine();
+            if (day == "Monday" || day == "Tuesday")
+            {
+                Console.WriteLine("Working day");
+            }
+            else if (day == "Wednesday" || day == "Thursday")
+            {
+                Console.WriteLine("Working day");
+            }
+            else if (day == "Friday")
+            {
+                Console.WriteLine("Working day");
+            }
+            else if (day == "Saturday" || day == "Sunday")
+            {
+                Console.WriteLine("Weekend");
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
         }
     }
 }
