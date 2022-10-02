d96bc8592e2cd1b0fbf0b86e53a8179a7b3f6370
GeorgePachedjiev <82244895+GeorgePachedjiev@users.noreply.github.com>
2022-10-02 03:01:35 -0900

    Fruit or Vegetables

--- Conditional Statements Advanced - Lab/01. Day of Week/08.Cinema Ticket/Program.cs
+++ Conditional Statements Advanced - Lab/01. Day of Week/08.Cinema Ticket/Program.cs
@@ -6,7 +6,32 @@ namespace _08.Cinema_Ticket
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Да се напише програма която чете ден от седмицата(текст) – въведен от потребителя и принтира на конзолата цената на билет за кино според деня от седмицата:
+            //Monday Tuesday Wednesday Thursday    Friday Saturday    Sunday
+            //12       12        14      14          12     16          16
+            //Примерен вход и изход
+            //вход изход       вход изход       вход изход
+            //Monday  12       Friday  12       Sunday  16
+
+            string dayOfWeek = Console.ReadLine();
+            switch (dayOfWeek)
+            {
+                case "Monday":
+                case "Tuesday":
+                case "Friday":
+                    Console.WriteLine(12);
+                    break;
+
+                case "Wednesday":
+                case "Thursday":
+                    Console.WriteLine(14);
+                    break;
+
+                case "Saturday":
+                case "Sunday":
+                    Console.WriteLine(16);
+                    break;
+            }
         }
     }
 }
