f9edc04af8609f44761914bff09cb14ff3281289
GeorgePachedjiev <82244895+GeorgePachedjiev@users.noreply.github.com>
2022-10-02 02:48:41 -0900

    DayOfWeek

---
+++ Conditional Statements Advanced - Lab/01. Day of Week/01. Day of Week/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01._Day_of_Week
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
