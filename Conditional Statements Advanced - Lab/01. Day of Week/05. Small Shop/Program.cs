5d659d6cec354e54da1f9252fd8051302c8b40ae
GeorgePachedjiev <82244895+GeorgePachedjiev@users.noreply.github.com>
2022-10-02 02:57:45 -0900

    SmallShop

--- Conditional Statements Advanced - Lab/01. Day of Week/05. Small Shop/Program.cs
+++ Conditional Statements Advanced - Lab/01. Day of Week/05. Small Shop/Program.cs
@@ -6,7 +6,98 @@ namespace _05._Small_Shop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //град / продукт  coffee   water   beer   sweets  peanuts
+            //Sofia            0.50    0.80    1.20    1.45    1.60
+            //Plovdiv          0.40    0.70    1.15    1.30    1.50
+            //Varna            0.45    0.70    1.10    1.35    1.55
+
+            string product = Console.ReadLine();
+            string city = Console.ReadLine();
+            double kolichestvo = double.Parse(Console.ReadLine());
+            double price = 0;
+            if (product == "coffee")
+            {
+                if (city == "Sofia")
+                {
+                    price = 0.50;
+                }
+                else if (city == "Plovdiv")
+                {
+                    price = 0.40;
+                }
+                else if (city == "Varna")
+                {
+                    price = 0.45;
+                }
+            }
+
+            if (product == "water")
+            {
+                if (city == "Sofia")
+                {
+                    price = 0.80;
+                }
+                else if (city == "Plovdiv")
+                {
+                    price = 0.70;
+                }
+                else if (city == "Varna")
+                {
+                    price = 0.70;
+                }
+            }
+
+            if (product == "beer")
+            {
+                if (city == "Sofia")
+                {
+                    price = 1.20;
+                }
+                else if (city == "Plovdiv")
+                {
+                    price = 1.15;
+                }
+                else if (city == "Varna")
+                {
+                    price = 1.10;
+                }
+            }
+
+            if (product == "sweets")
+            {
+                if (city == "Sofia")
+                {
+                    price = 1.45;
+                }
+                else if (city == "Plovdiv")
+                {
+                    price = 1.30;
+                }
+                else if (city == "Varna")
+                {
+                    price = 1.35;
+                }
+            }
+
+            if (product == "peanuts")
+            {
+                if (city == "Sofia")
+                {
+                    price = 1.60;
+                }
+                else if (city == "Plovdiv")
+                {
+                    price = 1.50;
+                }
+                else if (city == "Varna")
+                {
+                    price = 1.55;
+                }
+            }
+
+
+            double smetka = price * kolichestvo;
+            Console.WriteLine(smetka);
         }
     }
 }
