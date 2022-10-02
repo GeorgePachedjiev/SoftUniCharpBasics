5a03aea4efe8654b735a42d244fef13b35dbcb7b
GeorgePachedjiev <82244895+GeorgePachedjiev@users.noreply.github.com>
2022-10-02 03:00:41 -0900

    CinemaTickets

--- Conditional Statements Advanced - Lab/01. Day of Week/07.Working Hours/Program.cs
+++ Conditional Statements Advanced - Lab/01. Day of Week/07.Working Hours/Program.cs
@@ -6,7 +6,30 @@ namespace _07.Working_Hours
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int hour = int.Parse(Console.ReadLine());
+            string day = Console.ReadLine();
+            //Работи от понеделник до събота 10-18
+            if (hour >= 10 && hour <= 18)
+            {
+                switch (day)
+                {
+                    case "Monday":
+                    case "Tuesday":
+                    case "Wednesday":
+                    case "Thursday":
+                    case "Friday":
+                    case "Saturday":
+                        Console.WriteLine("open");
+                        break;
+                    default:
+                        Console.WriteLine("closed");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("closed");
+            }
         }
     }
 }
