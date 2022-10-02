81c1e9987cebd7c32ba7e563df16c780eca1a0b1
GeorgePachedjiev <82244895+GeorgePachedjiev@users.noreply.github.com>
2022-10-02 03:04:04 -0900

    TradeCommissions

--- Conditional Statements Advanced - Lab/01. Day of Week/11. Fruit Shop/Program.cs
+++ Conditional Statements Advanced - Lab/01. Day of Week/11. Fruit Shop/Program.cs
@@ -6,7 +6,83 @@ namespace _11._Fruit_Shop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string fruit = Console.ReadLine();
+            string day = Console.ReadLine();
+            double kolichestvo = double.Parse(Console.ReadLine());
+            double prize = 0;
+
+            if (day == "Monday" || day == "Tuesday" || day == "Wednesday"
+                || day == "Thursday" || day == "Friday")
+            {
+                switch (fruit)
+                {
+                    case "banana":
+                        prize = 2.50;
+                        break;
+                    case "apple":
+                        prize = 1.20;
+                        break;
+                    case "orange":
+                        prize = 0.85;
+                        break;
+                    case "grapefruit":
+                        prize = 1.45;
+                        break;
+                    case "kiwi":
+                        prize = 2.70;
+                        break;
+                    case "pineapple":
+                        prize = 5.50;
+                        break;
+                    case "grapes":
+                        prize = 3.85;
+                        break;
+                    default:
+                        Console.WriteLine("error");
+                        break;
+                }
+
+            }
+
+            else if (day == "Saturday" || day == "Sunday")
+            {
+                switch (fruit)
+                {
+                    case "banana":
+                        prize = 2.70;
+                        break;
+                    case "apple":
+                        prize = 1.25;
+                        break;
+                    case "orange":
+                        prize = 0.90;
+                        break;
+                    case "grapefruit":
+                        prize = 1.60;
+                        break;
+                    case "kiwi":
+                        prize = 3.00;
+                        break;
+                    case "pineapple":
+                        prize = 5.60;
+                        break;
+                    case "grapes":
+                        prize = 4.20;
+                        break;
+                    default:
+                        Console.WriteLine("error");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
+            double total = prize * kolichestvo;
+            if (total != 0)
+            {
+                Console.WriteLine($"{total:f2}");
+            }
         }
     }
 }
---
+++ Conditional Statements Advanced - Lab/01. Day of Week/12. Trade Commissions/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace _12._Trade_Commissions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string city = Console.ReadLine();
+            double prodashbi = double.Parse(Console.ReadLine());
+
+            double commision = 0;
+
+            if (prodashbi >= 0 && prodashbi <= 500)
+            {
+                if (city == "Sofia")
+                {
+                    commision = 0.05;
+                }
+                else if (city == "Varna")
+                {
+                    commision = 0.045;
+                }
+                else if (city == "Plovdiv")
+                {
+                    commision = 0.055;
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                }
+            }
+            else if (prodashbi > 500 && prodashbi <= 1000)
+            {
+                if (city == "Sofia")
+                {
+                    commision = 0.07;
+                }
+                else if (city == "Varna")
+                {
+                    commision = 0.075;
+                }
+                else if (city == "Plovdiv")
+                {
+                    commision = 0.08;
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                }
+            }
+            else if (prodashbi > 1000 && prodashbi <= 10000)
+            {
+                if (city == "Sofia")
+                {
+                    commision = 0.08;
+                }
+                else if (city == "Varna")
+                {
+                    commision = 0.1;
+                }
+                else if (city == "Plovdiv")
+                {
+                    commision = 0.12;
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                }
+            }
+            else if (prodashbi > 10000)
+            {
+                if (city == "Sofia")
+                {
+                    commision = 0.12;
+                }
+                else if (city == "Varna")
+                {
+                    commision = 0.13;
+                }
+                else if (city == "Plovdiv")
+                {
+                    commision = 0.145;
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                }
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
+
+            double zaplata = prodashbi * commision;
+            if (zaplata != 0)
+            {
+                Console.WriteLine($"{zaplata:f2}");
+            }
+        }
+    }
+}
