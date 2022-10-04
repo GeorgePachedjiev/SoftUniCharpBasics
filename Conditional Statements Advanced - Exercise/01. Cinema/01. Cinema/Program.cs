630862e6a1fda75d295aec5c4a917ce501cb145f
GeorgePachedjiev <82244895+GeorgePachedjiev@users.noreply.github.com>
2022-10-04 07:33:29 -0900

    Summers Outfits

--- Conditional Statements Advanced - Exercise/01. Cinema/01. Cinema/Program.cs
+++ Conditional Statements Advanced - Exercise/01. Cinema/01. Cinema/Program.cs
@@ -6,7 +6,28 @@ namespace _01._Cinema
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const double premiere = 12.00;
+            const double normal = 7.50;
+            const double discountdeca = 5.00;
+            double prize = 0;
+
+            string projectia = Console.ReadLine();
+            int red = int.Parse(Console.ReadLine());
+            int colona = int.Parse(Console.ReadLine());
+
+            if (projectia == "Premiere")
+            {
+                prize = premiere * red * colona;
+            }
+            else if (projectia == "Normal")
+            {
+                prize = normal * red * colona;
+            }
+            else if (projectia == "Discount")
+            {
+                prize = discountdeca * red * colona;
+            }
+            Console.WriteLine($"{prize:f2} leva");
         }
     }
 }
