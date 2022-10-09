0f2a481a28dd2e93fe99ff4fbc60b5521f1f42a6
GeorgePachedjiev <82244895+GeorgePachedjiev@users.noreply.github.com>
2022-10-09 05:58:25 -0900

    Odd and Even Sum

---
+++ For Loop - Lab/10. Odd Even Sum/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _10._Odd_Even_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double sumOddNumbers = 0;
+            double sumEvenNumbers = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int numbers = int.Parse(Console.ReadLine());
+
+                if (i % 2 == 0)
+                {
+                    sumEvenNumbers += numbers;
+                }
+                else
+                {
+                    sumOddNumbers += numbers;
+                }
+            }
+
+            double diff = Math.Abs(sumOddNumbers - sumEvenNumbers);
+
+            if (sumEvenNumbers == sumOddNumbers)
+            {
+                Console.WriteLine($"Yes");
+                Console.WriteLine($"Sum = {sumEvenNumbers}");
+            }
+
+            else
+            {
+                Console.WriteLine($"No");
+                Console.WriteLine($"Diff = {diff}");
+            }
+        }
+    }
+}
