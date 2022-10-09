baaaffdab50d1e74d0a840f0ff8ed76e30290c80
GeorgePachedjiev <82244895+GeorgePachedjiev@users.noreply.github.com>
2022-10-09 05:52:01 -0900

    09. Left and Right Sum

--- Conditional Statements Advanced - Exercise/01. Numbers from 1 to 100/08. Number sequence/Program.cs
+++ Conditional Statements Advanced - Exercise/01. Numbers from 1 to 100/08. Number sequence/Program.cs
@@ -6,7 +6,27 @@ namespace _08._Number_sequence
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numberCount = int.Parse(Console.ReadLine());
+            int max = int.MinValue;
+            int min = int.MaxValue;
+
+            for (int i = 0; i < numberCount; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (num > max)
+                {
+                    max = num;
+                }
+
+                if (num < min)
+                {
+                    min = num;
+                }
+
+            }
+            Console.WriteLine($"Max number: {max}");
+            Console.WriteLine($"Min number: {min}");
         }
     }
 }
