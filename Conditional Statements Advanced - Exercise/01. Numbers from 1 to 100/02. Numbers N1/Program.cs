227febd53b6342934ae6e7f72f89e975d590289d
GeorgePachedjiev <82244895+GeorgePachedjiev@users.noreply.github.com>
2022-10-09 05:47:38 -0900

    03. Numbers 1...N with Step 3

--- Conditional Statements Advanced - Exercise/01. Numbers from 1 to 100/02. Numbers N1/Program.cs
+++ Conditional Statements Advanced - Exercise/01. Numbers from 1 to 100/02. Numbers N1/Program.cs
@@ -6,7 +6,12 @@ namespace _02._Numbers_N1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = n; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
