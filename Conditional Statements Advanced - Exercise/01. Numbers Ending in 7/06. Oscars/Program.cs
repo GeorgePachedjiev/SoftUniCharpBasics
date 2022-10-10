ffc1b0ae86404422a68af440041231ccf80b1ceb
GeorgePachedjiev <82244895+GeorgePachedjiev@users.noreply.github.com>
2022-10-10 10:53:11 -0900

    Trekking Mania

--- Conditional Statements Advanced - Exercise/01. Numbers Ending in 7/06. Oscars/Program.cs
+++ Conditional Statements Advanced - Exercise/01. Numbers Ending in 7/06. Oscars/Program.cs
@@ -6,7 +6,36 @@ namespace _06._Oscars
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string nameOfActor = Console.ReadLine();
+            double academyPoints = double.Parse(Console.ReadLine());
+            int juryMembers = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < juryMembers; i++)
+            {
+
+                string nameOfJury = Console.ReadLine();
+                double points = double.Parse(Console.ReadLine());
+                points = ((nameOfJury.Length * points) / 2);
+
+                academyPoints += points;
+
+                if (academyPoints >= 1250.5)
+                {
+                    break;
+                }
+            }
+
+
+            if (academyPoints >= 1250.5)
+            {
+                Console.WriteLine($"Congratulations, {nameOfActor} got a nominee for leading role with {academyPoints:f1}!");
+            }
+
+            else
+            {
+                double diff = 1250.5 - academyPoints;
+                Console.WriteLine($"Sorry, {nameOfActor} you need {diff:f1} more!");
+            }
         }
     }
 }
