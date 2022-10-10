77d8e101aeca5a0ecbc914395eba60b605400f8b
GeorgePachedjiev <82244895+GeorgePachedjiev@users.noreply.github.com>
2022-10-10 10:51:39 -0900

    Salary

--- Conditional Statements Advanced - Exercise/01. Numbers Ending in 7/04. Clever Lily/Program.cs
+++ Conditional Statements Advanced - Exercise/01. Numbers Ending in 7/04. Clever Lily/Program.cs
@@ -6,7 +6,48 @@ namespace _04._Clever_Lily
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double lillyAge = int.Parse(Console.ReadLine());
+            double peralnqCena = double.Parse(Console.ReadLine());
+            double toyPrice = int.Parse(Console.ReadLine());
+            double budget = 0;
+            double toyCounter = 0;
+            double stake = 0;
+
+            for (int i = 1; i <= lillyAge; i++)
+            {
+
+                if (i % 2 == 0)
+                {
+
+
+                    stake += 10;
+                    budget = budget + stake;
+                    budget -= 1;
+                }
+
+                else
+                {
+                    toyCounter++;
+                }
+
+
+            }
+            double toyMoney = toyPrice * toyCounter;
+            budget += toyMoney;
+
+
+
+            if (budget >= peralnqCena)
+            {
+                double diff = budget - peralnqCena;
+                Console.WriteLine($"Yes! {diff:f2}");
+            }
+
+            else
+            {
+                double oshte = peralnqCena - budget;
+                Console.WriteLine($"No! {oshte:f2}");
+            }
         }
     }
 }
