050bd1420bf36f1a1b49a27389d9403d8dd62a11
GeorgePachedjiev <82244895+GeorgePachedjiev@users.noreply.github.com>
2022-10-10 10:50:09 -0900

    Histogram

--- Conditional Statements Advanced - Exercise/01. Numbers Ending in 7/02. Half Sum Element/Program.cs
+++ Conditional Statements Advanced - Exercise/01. Numbers Ending in 7/02. Half Sum Element/Program.cs
@@ -6,7 +6,38 @@ namespace _02._Half_Sum_Element
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            double sum = 0;
+            int maxNum = int.MinValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                int numbers = int.Parse(Console.ReadLine());
+
+                sum += numbers;
+
+                if (numbers > maxNum)
+                {
+                    maxNum = numbers;
+                }
+
+            }
+
+            double sumWithoutMaxNum = sum - maxNum;
+
+            if (sumWithoutMaxNum == maxNum)
+            {
+                Console.WriteLine($"Yes");
+                Console.WriteLine($"Sum = {sumWithoutMaxNum}");
+            }
+
+            else
+            {
+                double diff = Math.Abs(sumWithoutMaxNum - maxNum);
+
+                Console.WriteLine($"No");
+                Console.WriteLine($"Diff = {diff}");
+            }
         }
     }
 }
