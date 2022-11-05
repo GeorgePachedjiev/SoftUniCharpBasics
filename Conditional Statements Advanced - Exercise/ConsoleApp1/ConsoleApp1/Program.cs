18f7f42b470c1d5e511ab889b99588cc0d7aab04
GeorgePachedjiev <82244895+GeorgePachedjiev@users.noreply.github.com>
2022-11-05 04:59:10 -0900

    app3

---
+++ Conditional Statements Advanced - Exercise/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int n = int.Parse(Console.ReadLine());
+            //int[] array = new int[n];
+            //int counter = 0;
+            //double sum = 0;
+
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    array[i] = int.Parse(Console.ReadLine());
+
+            //    if (array[i] % 2 == 0)
+            //    {
+            //        counter++;
+            //    }
+            //    sum += array[i];
+            //}
+            //Console.WriteLine(sum);
+            //Console.WriteLine(counter);
+
+            //int[] array = Console.ReadLine()
+            //    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            //    .Select(int.Parse)
+            //    .ToArray();
+
+            //Console.WriteLine(String.Join(" || ", array));
+
+
+            int[] array = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            double counter = 0;
+            double sum = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+
+                if (array[i] % 2 == 0)
+                {
+                    counter++;
+                }
+                sum += array[i];
+            }
+            Console.WriteLine(counter);
+            Console.WriteLine(sum);
+        }
+    }
+}
