fecd1eff252a287d9c8f65ee1ef02517cf1a0165
GeorgePachedjiev <82244895+GeorgePachedjiev@users.noreply.github.com>
2022-11-05 04:59:56 -0900

    Create Program.cs

---
+++ Conditional Statements Advanced - Exercise/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int[] array = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(ArrayMinMaxSum(array));
+        }
+
+        static int ArrayMinMaxSum(int[] array)
+        {
+
+            int minN = int.MaxValue;
+            int maxN = int.MinValue;
+            int sum = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < minN)
+                {
+                    minN = array[i];
+                }
+                if(array[i] > maxN)
+                {
+                    maxN = array[i];
+                }
+                sum += array[i];
+            }
+            
+             return sum;
+
+            
+        }
+    }
+}
